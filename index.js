0aa331368d33d4d8dc7d256e8788dfffd9b43cc2
Shivek-Sharma24 <shiveksharma24@gmail.com>
2025-11-17 13:59:53 +0530

    several changes

--- index.js
+++ index.js
@@ -2,23 +2,31 @@ const express = require("express");
 const expenseRoutes = require("./routes/Expense");
 const cors = require("cors");
 const cookieparser = require("cookie-parser");
+
 const app = express();
 const port = process.env.port;
 
-const bodyParser = require("body-parser");
-app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
-});
 app.use(
   cors({
     origin: "https://cashflow-client.vercel.app",
-    credentials: true,
+    methods: "GET,POST,PUT,DELETE",
     allowedHeaders: ["Authorization", "Content-Type"],
   })
 );
-app.use(bodyParser.json());
-app.use(cookieparser());
-app.use("/", expenseRoutes);
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieparser());
+
+app.use("/", expenseRoutes);
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
+
+
+
+
+
