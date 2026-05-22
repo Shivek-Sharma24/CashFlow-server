7ffda2f9bf79cb65730d7eee25e39055c709ecfe
Shivek-Sharma24 <shiveksharma24@gmail.com>
2026-05-22 09:27:33 +0530

    change url

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const authRouter = require("./routes/authRoute.js");
 const bodyParser = require("body-parser");
 app.use(
   cors({
-    origin:"http://localhost:5173",
+    origin:"https://cash-flow-eta-mauve.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
